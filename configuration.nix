dbe621060dc98cfc03bb707ca9ebb319b4d32297
s-show <s1shimz@gmail.com>
2025-06-14 14:59:05 +0900

    不要な設定の削除

--- configuration.nix
+++ configuration.nix
@@ -5,7 +5,7 @@
 # NixOS-WSL specific options are documented on the NixOS-WSL repository:
 # https://github.com/nix-community/NixOS-WSL
 
-{ config, lib, pkgs, ... }:
+{ config, lib, pkgs, inputs, ... }:
 let
   hostname =
     if pkgs.system == "x86_64-linux" then
@@ -14,10 +14,6 @@ let
       "zenbook"
     else
       "generic";
-  # unstable-tarball = builtins.fetchTarball "https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz";
-  # unstable = import unstable-tarball {
-  #   config = config.nixpkgs.config;
-  # };
 in
 {
   networking.hostName = hostname;
@@ -102,7 +98,7 @@ in
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "24.11"; # Did you read the comment?
 
-  services.openssh.enable = true;
+  #services.openssh.enable = true;
 
   # Timezone
   time.timeZone = "Asia/Tokyo";
