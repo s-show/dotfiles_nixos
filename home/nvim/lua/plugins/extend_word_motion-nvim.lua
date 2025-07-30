df203d75826ea767cdc1e0f69019b336f9f4483c
s-show <s1shimz@gmail.com>
2025-07-30 23:30:01 +0900

    雑多な設定修正

--- home/nvim/lua/plugins/extend_word_motion-nvim.lua
+++ home/nvim/lua/plugins/extend_word_motion-nvim.lua
@@ -2,7 +2,6 @@ return {
     -- dir = '~/my_neovim_plugins/extend_word_motion.nvim',
   's-show/extend_word_motion.nvim',
   dev = false,
-  -- opts = {},
   opts = {
     -- extend_motions = { 'w', 'b', 'e', 'ge', 'f' },
     -- extend_modes = { 'n', 'v', 'o', 'c' }
