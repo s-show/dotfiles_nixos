dfeffa30b84f11983536988c8f5ca6ce91e876ab
s-show <s1shimz@gmail.com>
2026-04-17 00:58:20 +0900

    gitsigns.nvim と mini.test を導入

---
+++ home/nvim/lua/plugins/gitsigns-nvim.lua
@@ -0,0 +1,12 @@
+return {
+  "https://github.com/lewis6991/gitsigns.nvim",
+  opts = {
+    signs = {
+      add = { text = "+" },
+      change = { text = "~" },
+      delete = { text = "-" },
+      topdelete = { text = "‾" },
+      changedelete = { text = "~" },
+    },
+  }
+}
---
+++ home/nvim/lua/plugins/mini-test.lua
@@ -0,0 +1,8 @@
+return {
+  {
+    'nvim-mini/mini.nvim',
+    version = false,
+    lazy = true,
+    opts = {}
+  }
+}
