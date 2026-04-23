8027cc8b4420b7d7688784be5e29165b4af0d8a2
s-show <s1shimz@gmail.com>
2026-04-24 00:01:11 +0900

    ctrl-w w を alt w に変更

--- home/nvim/lua/setting/chowcho-nvim.lua
+++ home/nvim/lua/setting/chowcho-nvim.lua
@@ -44,13 +44,43 @@ chowcho.setup({
 })
 
 -- [Neovimの<C-w><C-w>をchowcho.nvimで拡張する](https://zenn.dev/kawarimidoll/articles/daa39da5838567)
-local win_keymap_set = function(key, callback)
-  vim.keymap.set({ 'n', 't' }, '<C-w>' .. key, callback)
-  vim.keymap.set({ 'n', 't' }, '<C-w><C-' .. key .. '>', callback)
-end
+-- local win_keymap_set = function(key, callback)
+--   vim.keymap.set({ 'n', 't' }, '<C-w>' .. key, callback)
+--   vim.keymap.set({ 'n', 't' }, '<C-w><C-' .. key .. '>', callback)
+-- end
 
-win_keymap_set('w',
-  function()
+-- win_keymap_set('w',
+--   function()
+--     local wins = 0
+--
+--     -- 全ウィンドウをループ
+--     for i = 1, vim.fn.winnr('$') do
+--       local win_id = vim.fn.win_getid(i)
+--       local conf = vim.api.nvim_win_get_config(win_id)
+--
+--       -- focusableなウィンドウをカウント
+--       if conf.focusable then
+--         wins = wins + 1
+--
+--         -- ウィンドウ数が3以上ならchowchoを起動
+--         if wins > 2 then
+--           chowcho.run()
+--           return
+--         end
+--       end
+--     end
+--
+--     -- ウィンドウが少なければ標準の<C-w><C-w>を実行
+--     vim.api.nvim_command('wincmd w')
+--   end
+-- )
+
+vim.keymap.set({ 'n', 't' }, "<M-w>", function()
+  local result, _ = pcall(require, 'chowcho')
+  if not result then
+    vim.api.nvim_command('wincmd w')
+    return
+  else
     local wins = 0
 
     -- 全ウィンドウをループ
@@ -70,7 +100,7 @@ win_keymap_set('w',
       end
     end
 
-    -- ウィンドウが少なければ標準の<C-w><C-w>を実行
+    -- ウィンドウが少なければ標準の<C-w>wを実行
     vim.api.nvim_command('wincmd w')
   end
-)
+end)
