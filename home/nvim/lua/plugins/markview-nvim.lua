7d8e8ee37126ea97ad6d49a93f518f3c31b57221
s-show <s1shimz@gmail.com>
2026-06-21 00:52:48 +0900

    Markdown プレビューツールの切り替えと設定変更

---
+++ home/nvim/lua/plugins/markview-nvim.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    'https://github.com/OXY2DEV/markview.nvim',
+    lazy = false,
+    -- Completion for `blink.cmp`
+    -- dependencies = { "saghen/blink.cmp" },
+    preview = {
+      -- icon_provider = "internal",   -- "mini" or "devicons"
+      icon_provider = "mini",   -- "mini" or "devicons"
+    }
+  }
+}
